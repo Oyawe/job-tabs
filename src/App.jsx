bcbe4380f6f3f9c254979d03f141260d74bd22d9
Oyawe <oyawegoodness@gmail.com>
2023-08-30 04:46:06 -0700

    added navigations(button tabs)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import JobInfo from "./JobInfo";
+import BtnContainer from "./BtnContainer";
 
 const url = "https://course-api.com/react-tabs-project";
 
@@ -29,6 +30,7 @@ const App = () => {
   return (
     <section className="jobs-center">
       {/* button coontainer */}
+      <BtnContainer jobs={jobs} />
       {/* job info */}
       <JobInfo jobs={jobs} />
     </section>
