f4d66e7765a195d47d6a5f2f995dfe0b235088a6
Oyawe <oyawegoodness@gmail.com>
2023-10-02 04:33:52 -0700

    cleaned up the code

--- src/Duties.jsx
+++ src/Duties.jsx
@@ -5,7 +5,7 @@ import { v4 as uuidv4 } from "uuid";
 const Duties = ({ duties }) => {
   return (
     <div>
-      {duties.map((duty, index) => {
+      {duties.map((duty) => {
         const id = uuidv4();
         // console.log(id );
         return (
